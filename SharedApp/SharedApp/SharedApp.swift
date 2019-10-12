5134e9dd4aa7174c9b20a9db068f782c510745e3
Joe Hinkle <joehinkle11@gmail.com>
2019-10-11 22:50:30 -0400

    interesting structure

--- SharedApp/SharedApp/SharedApp.swift
+++ SharedApp/SharedApp/SharedApp.swift
@@ -11,25 +11,59 @@ import Foundation
 public typealias FullStackObject = FullStackObjectClass & FullStackObjectPrototype
 
 public class FullStackObjectClass {
+    
+//    public classFullStackProperty
+    
+    public enum Permissions {
+        case read
+        case write
+        case create
+        case delete
+    }
+    
+    
+    public class FullStackProperty {
+        public let databasePropertyName: String
+        public let previousDatabasePropertyNames: [String]
+        public let isDeprecated: Bool
+        public let publicPermissions: [Permissions]
+        
+        public init(
+            databasePropertyName: String,
+            previousDatabasePropertyNames: [String] = [],
+            isDeprecated: Bool = false,
+            publicPermissions: [Permissions] = []
+        ) {
+            self.databasePropertyName = databasePropertyName
+            self.previousDatabasePropertyNames = previousDatabasePropertyNames
+            self.isDeprecated = isDeprecated
+            self.publicPermissions = publicPermissions
+        }
+    }
+    
     public init() {
 
     }
 }
 
 public protocol FullStackObjectPrototype {
-    static var Database_Collection_Name: String { get }
+    static var databaseCollectionName: String { get }
 }
 
 
 
 public class User: FullStackObject {
     // setup
-    static public var Database_Collection_Name = "User"
+    static public var databaseCollectionName = "User"
     
     // properties
-    public var username: String
+    public var username = FullStackProperty(
+        databasePropertyName: "username",
+        publicPermissions: [.read]
+    )
     
     public init(username: String) {
-        self.username = username
+//        self.username =
     }
 }
+
