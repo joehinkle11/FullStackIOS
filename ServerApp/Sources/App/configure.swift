f0916e153ef33238b344cbbdb14238bc8a8d6f90
Joe Hinkle <joehinkle11@gmail.com>
2019-10-26 20:45:43 -0400

    removed old database system, about to add couchdb for server

--- ServerApp/Sources/App/configure.swift
+++ ServerApp/Sources/App/configure.swift
@@ -1,11 +1,10 @@
 import Authentication
-import FluentSQLite
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
+//    try services.register(FluentSQLiteProvider())
     try services.register(AuthenticationProvider())
 
     // Register routes to the router
@@ -20,20 +19,20 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+//    // Configure a SQLite database
+//    let sqlite = try SQLiteDatabase(storage: .memory)
 
-    // Register the configured SQLite database to the database config.
-    var databases = DatabasesConfig()
-    databases.enableLogging(on: .sqlite)
-    databases.add(database: sqlite, as: .sqlite)
-    services.register(databases)
+//    // Register the configured SQLite database to the database config.
+//    var databases = DatabasesConfig()
+//    databases.enableLogging(on: .sqlite)
+//    databases.add(database: sqlite, as: .sqlite)
+//    services.register(databases)
 
-    /// Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: User.self, database: .sqlite)
-    migrations.add(model: UserToken.self, database: .sqlite)
-    migrations.add(model: Todo.self, database: .sqlite)
-    services.register(migrations)
+//    /// Configure migrations
+//    var migrations = MigrationConfig()
+//    migrations.add(model: User.self, database: .sqlite)
+//    migrations.add(model: UserToken.self, database: .sqlite)
+//    migrations.add(model: Todo.self, database: .sqlite)
+//    services.register(migrations)
 
 }
--- ServerApp/Sources/App/routes.swift
+++ ServerApp/Sources/App/routes.swift
@@ -5,23 +5,23 @@ import SharedApp
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // public routes
-    let userController = UserController()
-    router.post("users", use: userController.create)
+//    let userController = UserController()
+//    router.post("users", use: userController.create)
     
     let test = FullStackObjectClass()
-    let thing = SharedApp.User.Database_Collection_Name
+    let thing = SharedApp.User.databaseCollectionName
     router.get("test") { req in
         return thing
     }
     
     // basic / password auth protected routes
-    let basic = router.grouped(User.basicAuthMiddleware(using: BCryptDigest()))
-    basic.post("login", use: userController.login)
+//    let basic = router.grouped(User.basicAuthMiddleware(using: BCryptDigest()))
+//    basic.post("login", use: userController.login)
     
     // bearer / token auth protected routes
-    let bearer = router.grouped(User.tokenAuthMiddleware())
-    let todoController = TodoController()
-    bearer.get("todos", use: todoController.index)
-    bearer.post("todos", use: todoController.create)
-    bearer.delete("todos", Todo.parameter, use: todoController.delete)
+//    let bearer = router.grouped(User.tokenAuthMiddleware())
+//    let todoController = TodoController()
+//    bearer.get("todos", use: todoController.index)
+//    bearer.post("todos", use: todoController.create)
+//    bearer.delete("todos", Todo.parameter, use: todoController.delete)
 }
