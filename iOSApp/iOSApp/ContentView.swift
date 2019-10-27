02d745567331e9833ac530a6ffd24bab217de5d8
Joe Hinkle <joehinkle11@gmail.com>
2019-10-27 17:55:08 -0400

    crude ui working

--- iOSApp/iOSApp/ContentView.swift
+++ iOSApp/iOSApp/ContentView.swift
@@ -20,40 +20,61 @@ let user = User(username: "john")
 struct ContentView: View {
     @State var message = ""
     @State var username = "some"
-    @State private var selection = 1
+    @State private var selection = 0
     
-//    private
+    let documentOptions: [(String, FullStackObject.Type?)] = [
+        ("None", nil),
+        ("User", User.self)
+    ]
     
     var body: some View {
         VStack {
-            TextField("username", text: $username)
-                .frame(width: 100)
-            
-            
-            Picker(selection: $selection, label: Text("Document Key")) {
-                Text("User").tag(1)
-                Text("Other?").tag(2)
-            }
-
-            Button(action: {
-                self.message = "test"
-            }) {
-                Text("Add document!")
-                .fontWeight(.bold)
+            Text("Welcome to the Full Stack iOS demo!")
+                .bold()
                 .font(.title)
-                .padding()
-                .background(Color.purple)
-                .cornerRadius(40)
-                .foregroundColor(.white)
-                .padding(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 40)
-                        .stroke(Color.purple, lineWidth: 5)
-                )
+            Divider()
+            Spacer()
+            
+            Picker(selection: $selection, label: Text("Select a document")) {
+                ForEach(0 ..< documentOptions.count) {
+                    Text(self.documentOptions[$0].0).tag($0)
+                }
             }
             
-            
-            Text(message)
+            selectionView()
+        }
+    }
+    
+    func selectionView() -> AnyView {
+        if let selectedDocumentType = documentOptions[selection].1 {
+            return AnyView(VStack {
+                Text("Selection: \(documentOptions[selection].0)")
+                
+                TextField("username", text: $username)
+                    .frame(width: 100)
+                Button(action: {
+                    self.message = "test"
+                }) {
+                    Text("Add document!")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .padding()
+                    .background(Color.purple)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 40)
+                            .stroke(Color.purple, lineWidth: 5)
+                    )
+                }
+                
+                
+                Text(message)
+                Spacer()
+            })
+        } else {
+            return AnyView(Spacer())
         }
     }
 }
